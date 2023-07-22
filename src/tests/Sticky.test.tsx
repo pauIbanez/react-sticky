7ac53260a57402be77baacd5d171cd6372f3c913
Pau Ibáñez <pauibanez2001@gmail.com>
2023-07-22 14:26:46 +0200

    updated file structure

--- src/tests/Sticky.test.tsx
+++ src/tests/Sticky.test.tsx
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
-import Stickyy from "./Sticky";
+import Stickyy from "../Sticky";
 
 describe("Given the Sticky component", () => {
   describe("When it's instanciated with no scroll", () => {
